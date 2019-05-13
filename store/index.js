67840b2a3abf054f67d07f79e238c93a0078d85c
Aydin <ahepsaydir@and.digital>
2019-05-13 15:53:26 +0100

    tied vuex to firebase, the app pre-renders now

--- store/index.js
+++ store/index.js
@@ -9,6 +9,15 @@ const createStore = () => {
     mutations: {
       setPosts(state, posts) {
         state.loadedPosts = posts;
+      },
+      addPost(state, post) {
+        state.loadedPosts.push(post);
+      },
+      editPost(state, editedPost) {
+        const postIndex = state.loadedPosts.findIndex(
+          post => post.id === editedPost.id
+        );
+        state.loadedPosts[postIndex] = editedPost;
       }
     },
     actions: {
@@ -26,6 +35,36 @@ const createStore = () => {
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit("setPosts", posts);
+      },
+      addPost(vuexContext, post) {
+        const createdPost = {
+          ...post,
+          updatedDate: new Date()
+        };
+        return axios
+          .post("https://foodie-news.firebaseio.com/posts.json", {
+            ...createdPost
+          })
+          .then(res => {
+            vuexContext.commit("addPost", {
+              ...createdPost,
+              id: res.data.name
+            });
+          })
+          .catch(e => context.error(e));
+      },
+      editPost(vuexContext, editedPost) {
+        return axios
+          .put(
+            "https://foodie-news.firebaseio.com/posts/" +
+              editedPost.id +
+              ".json",
+            editedPost
+          )
+          .then(() => {
+            vuexContext.commit("editPost", editedPost);
+          })
+          .catch(e => context.error(e));
       }
     },
     getters: {
